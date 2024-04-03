3a36bb5e4b61c83386fbbbae11b127e6a19219e3
enzo filippo <enzo.filippo23@gmail.com>
2024-04-03 15:30:42 +1300

    ui

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Roboto"].concat(defaultTheme.fontFamily.mono),
+      },
+      colors: {
+        "background-dark": "#1E1B16",
+        "heliner-orange": "#F39800",
+      },
+    },
+  },
+  plugins: [],
+};
